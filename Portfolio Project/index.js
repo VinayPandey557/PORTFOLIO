69043b0b776a31af73edf451491b2454acb12885
Vinay Pandey <vinaypandey46158@gmail.com>
2024-02-25 18:16:06 +0530

    First commit

---
+++ Portfolio Project/index.js
@@ -0,0 +1,16 @@
+let dayNight = document.querySelector(".dayNight");
+let banner = document.querySelector(".banner");
+
+dayNight.addEventListener("click", ()=>{
+    banner.classList.toggle("night");
+})
+
+
+let typingEffect = new Typed("#text", {
+    strings:["Portfolio","Student","Coder","Vinay Kumar Pandey"],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:50,
+    backDelay:1000
+})
+
